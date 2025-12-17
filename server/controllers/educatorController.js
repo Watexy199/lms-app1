93cce77cc5710215ae9e1c728fc4c4a70de52c9d
Talent Mandishe <talentymandishe@gmail.com>
2025-12-17 04:17:57 +0200

    server changes

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,7 +1,7 @@
 import {clerkClient} from "@clerk/express"
 
 
-export const updateRoleToEducator = async (req,res)=>{
+export const updateRoleToEducator = async (req, res)=>{
     try {
         const userId = req.auth.userId
 
