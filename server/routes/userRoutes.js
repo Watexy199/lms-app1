4294e624471ccb8c98fbb8a7c6538df7733ab12e
Talent Mandishe <talentymandishe@gmail.com>
2026-01-02 02:49:51 +0200

    backend added

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,7 @@
 
 
 import express from "express";
-import { getUserData, purchaseCourse, userEnrolledCourses } from "../controllers/userController.js";
+import { addUserRating, getUserCourseProgress, getUserData, purchaseCourse, updateUserCourseProgress, userEnrolledCourses } from "../controllers/userController.js";
 import { requireAuth } from "@clerk/express";
 
 const userRouter = express.Router();
@@ -9,5 +9,9 @@ const userRouter = express.Router();
 userRouter.get("/data", requireAuth(), getUserData);
 userRouter.get("/enrolled-courses", requireAuth(), userEnrolledCourses);
 userRouter.post("/purchase", requireAuth(), purchaseCourse);
+userRouter.post("/update-course-progress", updateUserCourseProgress)
+userRouter.post("/get-course-progress", getUserCourseProgress)
+userRouter.post("/add-rating", addUserRating)
+
 
 export default userRouter;
